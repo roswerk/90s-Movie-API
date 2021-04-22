2018b48ece1f3f8bd33f49244a7dd0102b17d26f
Max <73542409+roswerk@users.noreply.github.com>
2021-04-22 14:31:02 +0200

    Updated status code 401

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -22,7 +22,7 @@ module.exports = (router) => {
   router.post("/login", (req, res) => {
     passport.authenticate("local", {session: false}, (error, user, info) =>{
       if(error || !user) {
-        return res.status(400).json({
+        return res.status(401).json({
           message: "Something is not right",
           user: user
         });
@@ -33,6 +33,8 @@ module.exports = (router) => {
         }
         let token = generateJWTToken(user.toJSON());
         return res.json({user, token});
+
+
       });
     })(req, res);
   });
