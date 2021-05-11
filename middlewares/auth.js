ca092745db1329e2414afe4ae7a89b3dfa3145c0
Max <73542409+roswerk@users.noreply.github.com>
2021-05-11 20:51:52 +0200

    Updated login Auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,46 +1,93 @@
-const jwtSecret = "your_jwt_secret"; //This has to be
-//the same key used in the JWTStrategy
+//const jwtSecret = "your_jwt_secret"; //This has to be
+////the same key used in the JWTStrategy
+//
+//const jwt = require("jsonwebtoken");
+//const passport = require("passport");
+//
+//require("../helpers/passport"); //Local passport file
+//
+//let generateJWTToken = (user) => {
+//  return jwt.sign(user, jwtSecret, {
+//    subject: user.userName, //This is the username
+//    //we're encoding in the JWT
+//    expiresIn: "7d", //This specifies that
+//    //the token will expire in 7 days
+//    algorithm: "HS256" //This is the algorithm used
+//    //to "sign" or encode the values of the JWT
+//  });
+//}
+//
+////POST Login
+//module.exports = (router) => {
+//  router.post("/login", (req, res) => {
+//
+//    passport.authenticate("local", {session: true}, (error, user) =>{
+//      if(error || !user) {
+//        return res.status(401).json({
+//          message: "Something is not right",
+//          user: user
+//        });
+//      }
+//      req.login(user, {session: true}, (error) => {
+//        if(error){
+//          res.send(error);
+//        }
+//        var userObj = user.toObject();
+//        // delete userObj.password;
+//        // delete userObj.birthDate;
+//        let token = generateJWTToken(user.toJSON());
+//        return res.json({userObj, token});
+//        // return res.redirect("/movies")
+//
+//
+//      });
+//    })(req, res);
+//  });
+//}
 
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
 
-require("../helpers/passport"); //Local passport file
 
-let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
-    subject: user.userName, //This is the username
-    //we're encoding in the JWT
-    expiresIn: "7d", //This specifies that
-    //the token will expire in 7 days
-    algorithm: "HS256" //This is the algorithm used
-    //to "sign" or encode the values of the JWT
-  });
-}
 
-//POST Login
-module.exports = (router) => {
-  router.post("/login", (req, res) => {
 
-    passport.authenticate("local", {session: true}, (error, user) =>{
-      if(error || !user) {
-        return res.status(401).json({
-          message: "Something is not right",
-          user: user
-        });
-      }
-      req.login(user, {session: true}, (error) => {
-        if(error){
-          res.send(error);
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const Models = require('../database/models');
+
+const Users = Models.user;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy(
+    {
+      usernameField: 'userName',
+      passwordField: 'password',
+    },
+    (username, password, callback) => {
+      Users.findOne({userName: username}, (error, user) => {
+        if (error) {
+          return callback(error);
         }
-        var userObj = user.toObject();
-        // delete userObj.password;
-        // delete userObj.birthDate;
-        let token = generateJWTToken(user.toJSON());
-        return res.json({userObj, token});
-        // return res.redirect("/movies")
-
-
+        if (!user) {
+          return callback(null, false, 'Incorrect username or password.');
+        }
+        if (!user.validatePassword(password)) {
+          return callback(null, false, 'Incorrect password.');
+        }
+        return callback(null, user);
       });
-    })(req, res);
-  });
-}
+    },
+));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'secret_jwt'}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+      .then((user) => {
+        return callback(null, user);
+      })
+      .catch((error) => {
+        return callback(error);
+      });
+}));
