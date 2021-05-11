9270c92f9b287c75ba089ef51d8303395dcd6515
Max <73542409+roswerk@users.noreply.github.com>
2021-05-11 20:01:39 +0200

    Updated login auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -21,14 +21,14 @@ let generateJWTToken = (user) => {
 module.exports = (router) => {
   router.post("/login", (req, res) => {
 
-    // passport.authenticate("local", {session: false}, (error, user) =>{
-    //   if(error || !user) {
-    //     return res.status(401).json({
-    //       message: "Something is not right",
-    //       user: user
-    //     });
-    //   }
-      req.login(user, {session: false}, (error) => {
+    passport.authenticate("local", {session: true}, (error, user) =>{
+      if(error || !user) {
+        return res.status(401).json({
+          message: "Something is not right",
+          user: user
+        });
+      }
+      req.login(user, {session: true}, (error) => {
         if(error){
           res.send(error);
         }
@@ -36,11 +36,11 @@ module.exports = (router) => {
         // delete userObj.password;
         // delete userObj.birthDate;
         let token = generateJWTToken(user.toJSON());
-        // return res.json({userObj, token});
-        return res.redirect("/movies")
+        return res.json({userObj, token});
+        // return res.redirect("/movies")
 
 
       });
-    // })(req, res);
+    })(req, res);
   });
 }
